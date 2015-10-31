339294baa75ed5a4dee1bb7ae8efba46e09693b3
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2015-10-31 22:04:21 +0100

    magiczna poprawka

--- app/autoload.php
+++ app/autoload.php
@@ -5,13 +5,14 @@ use Composer\Autoload\ClassLoader;
 
 $environmentType = getenv('ENVIRONMENT_TYPE');
 
+/** @var ClassLoader $loader */
 if ($environmentType == 'dev') {
     $loader = require '/home/vagrant/backend/vendor/autoload.php';
+    $loader->addPsr4('', '/vagrant/src');
 } else {
     $loader = require __DIR__ . '/../vendor/autoload.php';
 }
 
-/** @var ClassLoader $loader */
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 return $loader;
